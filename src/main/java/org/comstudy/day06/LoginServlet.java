2e1db0453f8ac24a5eb686ea79260d6939369408
origin <comstudynews@gmail.com>
2024-05-28 13:44:37 +0900

    session study 03

--- src/main/java/org/comstudy/day06/LoginServlet.java
+++ src/main/java/org/comstudy/day06/LoginServlet.java
@@ -14,10 +14,19 @@ import javax.servlet.http.HttpSession;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	HttpSession session;
 	private String viewName = "";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("doGet - LoginServlet");
+		
+		session = request.getSession();
+		
+		// 기존에 로그인 된 정보 초기화 하기 (logout)
+		// 속성을 일일이 지우기
+		session.removeAttribute("username");
+		// 전체 session을 한번에 초기화 
+		session.invalidate();
 		
 		viewName = "/WEB-INF/views/member/login.jsp";
 		dispatcher(request, response);
@@ -25,10 +34,15 @@ public class LoginServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("doPost - LoginServlet");
+		// 파라미터 정보를 가져 온다.
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 		
-		// 로그인이 되면 session에 로그인 정보 바인딩하기
-		HttpSession session = request.getSession();
-		session.setAttribute("username", "Hong");
+		if("user01".equals(username) && "12345".equals(password)) {
+			// 로그인이 되면 session에 로그인 정보 바인딩하기
+			session = request.getSession();
+			session.setAttribute("username", username);
+		}
 		
 		viewName = "redirect:" + request.getContextPath() + "/jsp/welcome.jsp";
 		System.out.println("viewName >>> " + viewName);
